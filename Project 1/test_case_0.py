d8cd66e411b10c023d84f3fdd8301e128523c17e
Eric9867 <32530980+Eric9867@users.noreply.github.com>
2020-10-08 13:40:19 -0400

    * created two test cases based on qilin and mak13... 's code on discord server :zany_face: * test_case_1 has encountered issues that should be addressed in the next commit

---
+++ Project 1/test_case_0.py
@@ -0,0 +1,101 @@
+from gamify import *
+
+if __name__ == "__main__":
+    initialize()
+    offer_star("textbooks")
+    print(most_fun_activity_minute())
+    perform_activity("running", 120)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    print(most_fun_activity_minute())
+    perform_activity("textbooks", 140)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("running", 120)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    offer_star("running")
+    perform_activity("resting", 80)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("resting", 80)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    print(most_fun_activity_minute())
+    perform_activity("textbooks", 30)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("textbooks", 80)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("running", 110)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("textbooks", 60)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    print(most_fun_activity_minute())
+    offer_star("textbooks")
+    offer_star("running")
+    perform_activity("running", 50)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    offer_star("running")
+    print(most_fun_activity_minute())
+    perform_activity("textbooks", 110)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("resting", 130)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("resting", 10)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("resting", 20)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    offer_star("running")
+    perform_activity("textbooks", 50)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    print(most_fun_activity_minute())
+    offer_star("textbooks")
+    print(most_fun_activity_minute())
+
+#  textbooks
+#  360
+#  -200
+#  resting
+#  640
+#  -480
+#  1000
+#  -720
+#  1000
+#  -720
+#  1000
+#  -720
+#  running
+#  1060
+#  -710
+#  1220
+#  -870
+#  1550
+#  -1090
+#  1670
+#  -1210
+#  resting
+#  1820
+#  -1280
+#  resting
+#  2040
+#  -1500
+#  2040
+#  -1500
+#  2040
+#  -1500
+#  2040
+#  -1500
+#  2140
+#  -1510
+#  resting
+#  resting
---
+++ Project 1/test_case_1.py
@@ -0,0 +1,117 @@
+from gamify import *
+
+if __name__ == "__main__":
+    initialize()
+    print(most_fun_activity_minute())
+    perform_activity("running", 120)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("resting", 100)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    offer_star("textbooks")
+    perform_activity("textbooks", 120)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("textbooks", 40)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    print(most_fun_activity_minute())
+    perform_activity("textbooks", 90)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    print(most_fun_activity_minute())
+    perform_activity("running", 140)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    offer_star("running")
+    perform_activity("textbooks", 60)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("resting", 120)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("running", 60)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    print(most_fun_activity_minute())
+    offer_star("textbooks")
+    perform_activity("running", 60)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("running", 100)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    perform_activity("resting", 40)
+    print(get_cur_health())
+    print(get_cur_hedons())
+
+#1980
+#-1470
+
+    print()
+    print(most_fun_activity_minute())
+#  resting
+    perform_activity("running", 110)
+    print(get_cur_health())
+    print(get_cur_hedons())
+#  2310
+#  -1690
+    perform_activity("textbooks", 120)
+    print(get_cur_health())
+    print(get_cur_hedons())
+#  2550
+#  -1930
+    perform_activity("resting", 60)
+    print(get_cur_health())
+    print(get_cur_hedons())
+#  2550
+#  -1930
+    offer_star("running")
+    perform_activity("textbooks", 130)
+    print(get_cur_health())
+    print(get_cur_hedons())
+    offer_star("running")
+#  2810
+#  -2190
+
+
+# @Qcumber#4444 and @mak13789#4418's code achieved the following response:
+#  running
+#  360
+#  -200
+#  360
+#  -200
+#  600
+#  -410
+#  680
+#  -490
+#  resting
+#  860
+#  -670
+#  resting
+#  1280
+#  -950
+#  1400
+#  -1070
+#  1400
+#  -1070
+#  1580
+#  -1150
+#  resting
+#  1760
+#  -1270
+#  1980
+#  -1470
+#  1980
+#  -1470
+
+#  resting
+#  2310
+#  -1690
+#  2550
+#  -1930
+#  2550
+#  -1930
+#  2810
+#  -2190
