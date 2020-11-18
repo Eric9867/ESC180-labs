c081e235b6e52b24233acc324bf2bd0ce1462185
Eric9867 <32530980+Eric9867@users.noreply.github.com>
2020-11-18 13:20:27 -0500

    * created file for lab08 * problem 1 draft written

---
+++ 8/lab08.py
@@ -0,0 +1,17 @@
+
+
+## Problem 1
+def print_lol(file_location):
+    file = open(file_location)
+    text = file.readlines()
+    for line in text:
+        if 'lol' in line.lower():
+            print(line)
+
+## Problem 2
+def 
+
+
+
+if __name__ == "__main__":
+    pass
