8c8b266af2881f556c80b357276f87ff96eb7e91
Eric9867 <32530980+Eric9867@users.noreply.github.com>
2020-11-25 16:11:39 -0500

    * lab09 complete * html file and text file (Pride_and_Prejudice) for python exercises

---
+++ 9/lab09.py
@@ -0,0 +1,151 @@
+import urllib.request
+#import BeautifulSoup
+
+#####
+# Problem 1
+
+def file_word_list(path):
+    '''
+    Reads the text file at path and return a 
+    list of strings that were orgininally separated by
+    punctuation (characters in remove_chars) 
+    '''
+    remove_chars = [',', '-', '.', '!', '?', ';', ':', '\n', '\t', '*', '(', ')', '"']
+    
+    file = open(path, encoding="latin")
+    text = file.read().lower()
+    #text = "I am a sick man. I am a spiteful man. I am an unattractive man. I believe my liver is diseased.\nHowever, I know nothing at all about my disease, and do not know for certain what ails me"
+    file.close()
+    for char in remove_chars:
+        text = text.replace(char, ' ')
+    return text.split()
+
+# part (a)
+def word_frequency(words):
+    word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        if word not in word_counts:
+            word_counts[word] = 1
+    return word_counts
+
+# (b)
+def top10(L):
+    '''
+    Returns a list of the 10 largest integers in the list L
+    '''
+    assert type(L) == list
+    
+    largest_nums = [0] * 10
+    for n in L:
+        if n > min(largest_nums):
+            largest_nums[0] = n
+            largest_nums.sort()
+    return largest_nums
+
+# part (C)
+def inv_freq(freq):
+    """
+    docstring
+    """
+    val_list = []
+    freq_arr = []
+    inv_dict = {}
+    for key, value in freq.items():
+        if value not in inv_dict:
+            inv_dict[value] = key
+        else:
+            freq_arr.append([key,value])
+
+        val_list.append(value)
+    max_vals = top10(val_list)
+    print("max_vals: ",max_vals)
+    top_ten = {}
+    for value in max_vals:
+        if inv_dict[value] not in top_ten:
+             top_ten[inv_dict[value]] = value
+        else:
+            for [key, arr_value] in freq_arr:
+                if arr_value == value and key not in top_ten:
+                    top_ten[key] = value 
+                    break
+
+    return top_ten
+
+
+
+#####
+# Problem 2
+
+# • Make the word “ever” appear in bold
+def bold_word(path, word):
+    file = open(path, 'r+')
+    file_text = file.read()
+    file_text = file_text.replace(word, '<strong>' + word + '</strong>')
+    file.seek(0)
+    file.write(file_text)
+    file.close()
+    return None
+
+# • Add a link to Yahoo.ca’s search results for “engineering science.”
+def search_eng_sci(path):
+    '''
+    Adds a link to a Yahoo search query for engineering science
+    to the last empty line of the html file at path
+    '''
+    url = 'https://ca.search.yahoo.com/search;_ylt=AwrJ6y6Yu75f9F4ALFrqFAx.;_ylc=X1MDMjExNDcyMTAwMgRfcgMyBGZyAwRncHJpZAN5X3BXRm5kLlNneUM1TWtseVMyWTFBBG5fcnNsdAMwBG5fc3VnZwM5BG9yaWdpbgNjYS5zZWFyY2gueWFob28uY29tBHBvcwMwBHBxc3RyAwRwcXN0cmwDBHFzdHJsAzIxBHF1ZXJ5A2VuZ2luZWVyaW5nJTIwc2NpZW5jZQR0X3N0bXADMTYwNjMzNTM4OQ--?fr2=sb-top-ca.search&p=engineering+science&fr=sfp&iscqry='
+    file = open(path, 'r+')
+    file_lines = file.readlines()
+    
+    for i in range(len(file_lines)-1,0,-1):
+        if file_lines[i] == '<p><a href="' +url+ '">Engineering Science</p>':
+            break
+        elif file_lines[i] == '\n':
+            file_lines[i] = '<p><a href="' +url+ '">Engineering Science</p>\n'
+            break
+    
+    file.seek(0)
+    file.writelines(file_lines)
+# Print statements for debugging
+    # print(file_lines)
+    # file.seek(0)
+    # print(file.read())
+    file.close()
+    return None
+
+
+#######
+# Problem 3
+
+def num_results(search_term):
+    search_term = search_term.replace(" ","+")
+    f = urllib.request.urlopen("https://ca.search.yahoo.com/search?p="+search_term+"&fr=yfp-t&fp=1&toggle=1&cop=mss&ei=UTF-8")
+    page = f.read().decode("utf-8")
+    f.close()
+    # print(page[:len(page)//4])
+    # print(page[len(page)//4:len(page)//2])
+    # print(page[len(page)//2:len(page)*3//4])
+    # print(page[len(page)*3//4:len(page)])
+    string = 'referrerpolicy="unsafe-url">Next<ins></ins></a><span>'
+    index_val = page.find(string)
+
+    res = page[index_val + len(string) : index_val + len(string)+50].split()[0]
+    print(res, "results") 
+
+    # soup = BeautifulSoup(page, "html.parser")
+    # value = soup.find("input", {"name":"form1"}.get("Results"))
+
+if __name__ == "__main__":
+    print("\n======================================================================================================\nPROBLEM 1 TESTS")
+    # word_list = file_word_list('9\\Pride_and_Prejudice.txt')
+    # print(len(word_list))
+    # word_counts = word_frequency(word_list)
+    # print(word_counts)
+    
+    # print("top ten words:", inv_freq(word_counts))
+    # print("\n======================================================================================================\nPROBLE 2 TESTS")
+    # bold_word('9\\Hello_World - Copy.html', 'ever')    
+    # search_eng_sci('9\\Hello_World - Copy.html')    
+    # print("\n======================================================================================================\nPROBLEM 3 TESTS")
+    num_results("flathead screwdriver")
