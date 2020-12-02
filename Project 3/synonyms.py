268aef08ba88680e9979718b47666643b687a022
Halil Kelebek <halilsina.kelebek@gmail.com>
2020-12-02 13:48:49 -0500

    wrote build_semantic_descriptor

--- Project 3/synonyms.py
+++ Project 3/synonyms.py
@@ -39,9 +39,42 @@ def cosine_similarity(vec1, vec2):
 
 
 def build_semantic_descriptors(sentences):
+    semantic_dict = {}
+    word_count = {}
+    for sentence in sentences:                              # called len(sentences) times
+        for i in range(len(sentence)):                      # finding the word count of in a sentence# called len(sentence) times
+            # if sentence[i] not in semantic_dict:
+            #     semantic_dict[sentence[i]] =  {}
+            if sentence[i] not in word_count:
+                word_count[sentence[i]] = 1
+            else:
+                word_count[sentence[i]] += 1
+                
+        for i in range(len(sentence)):
+            dict_copy = dict(word_count)
+            if sentence[i] in dict_copy: dict_copy.pop(sentence[i])
+            if sentence[i] not in semantic_dict:
+                semantic_dict[sentence[i]] = dict_copy  
+            else:
+                for key, value in dict_copy.items():
+                    if key in semantic_dict[sentence[i]]:
+                        semantic_dict[sentence[i]][key] += value
+                    else:
+                        semantic_dict[sentence[i]][key] = value
+        word_count = {}                       
+    return semantic_dict
 
-    pass
+# def build_semantic_descriptors(sentences):
+#     semantic_dict = {}
+#     word_count = {}
+#     for sentence in sentences:
+#         for i in range(len(sentence)):
+#             if sentence[i] not in semantic_dict:
+#                 semantic_dict[sentence[i]] =  {}
+#             for i in range(len(sentence)):
+                
 
+#     pass
 def build_semantic_descriptors_from_files(filenames):
     pass
 
@@ -55,4 +88,13 @@ def run_similarity_test(filename, semantic_descriptors, similarity_fn):
     pass
 
 if __name__ == "__main__":
+    sentences = [["i", "am", "a", "sick", "man"],
+                 ["i", "am", "a", "spiteful", "man"],
+                 ["i", "am", "an", "unattractive", "man"],
+                 ["i", "believe", "my", "liver", "is", "diseased"],
+                 ["however", "i", "know", "nothing", "at", "all", "about", "my",
+                 "disease", "and", "do", "not", "know", "for", "certain", "what", "ails", "me"]]
+    
     print(cosine_similarity({"a": 1, "b": 2, "c": 3}, {"b": 4, "c": 5, "d": 6}))
+    print(build_semantic_descriptors(sentences)['man'])
+    print(build_semantic_descriptors(sentences)['liver'])
