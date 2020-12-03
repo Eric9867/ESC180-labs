2570286240c051c95fe28ae2390c6838e33f1c8b
Eric9867 <32530980+Eric9867@users.noreply.github.com>
2020-12-03 15:03:44 -0500

    * fixed calling dictionnary with a non-existant key * fixed separating sentences by '\n'

--- Project 3/synonyms.py
+++ Project 3/synonyms.py
@@ -31,35 +31,11 @@ def cosine_similarity(vec1, vec2):
     
     for key, value in vec2.items():
         vec2_mag_squared += value**2
-    vec2_mag = math.sqrt(vec2_mag_squared)
 
+    vec2_mag = math.sqrt(vec2_mag_squared)
     sim_vec1_vec2 = dot_prod/(vec1_mag*vec2_mag)
 
     return sim_vec1_vec2
-
-# def build_semantic_descriptors(sentences):
-#     semantic_dict = {}
-#     word_count = {}
-#     for sentence in sentences:                              # called len(sentences) times
-#         for i in range(len(sentence)):                      # finding the word count of in a sentence# called len(sentence) times
-#             if sentence[i] not in word_count:
-#                 word_count[sentence[i]] = 1
-#             else:
-#                 word_count[sentence[i]] += 1
-                
-#         for i in range(len(sentence)):
-#             dict_copy = dict(word_count)
-#             if sentence[i] in dict_copy: dict_copy.pop(sentence[i])
-#             if sentence[i] not in semantic_dict:
-#                 semantic_dict[sentence[i]] = dict_copy  
-#             else:
-#                 for key, value in dict_copy.items():
-#                     if key in semantic_dict[sentence[i]]:
-#                         semantic_dict[sentence[i]][key] += value
-#                     else:
-#                         semantic_dict[sentence[i]][key] = value
-#         word_count = {}
-#     return semantic_dict
 
 def build_semantic_descriptors(sentences):
     semantic_dict = {}
@@ -76,47 +52,8 @@ def build_semantic_descriptors(sentences):
                         semantic_dict[duplicates_removed[i]][word] = 1
                     else:
                         semantic_dict[duplicates_removed[i]][word] += 1
-    # for sentence in sentences:                              # called len(sentences) times
-    #     for i in range(len(sentence)):                      # finding the word count of in a sentence# called len(sentence) times
-    #         if sentence[i] not in word_count:
-    #             word_count[sentence[i]] = 1
-    #         else:
-    #             word_count[sentence[i]] += 1
-                
-    #     for i in range(len(sentence)):
-    #         dict_copy = dict(word_count)
-    #         if sentence[i] in dict_copy: dict_copy.pop(sentence[i])
-    #         if sentence[i] not in semantic_dict:
-    #             semantic_dict[sentence[i]] = dict_copy  
-    #         else:
-    #             for key, value in dict_copy.items():
-    #                 if key in semantic_dict[sentence[i]]:
-    #                     semantic_dict[sentence[i]][key] += value
-    #                 else:
-    #                     semantic_dict[sentence[i]][key] = value
-    #     word_count = {}
+    # print(semantic_dict)
     return semantic_dict
-
-# def build_semantic_descriptors(sentences):
-#     semantic_dict = {}
-#     word_count = {}
-#     for sentence in sentences:                              # called len(sentences) times
-#         for i in range(len(sentence)):                      # finding the word count of in a sentence# called len(sentence) times
-#             if sentence[i] not in word_count:
-#                 word_count[sentence[i]] = 1
-#             if sentence[i] not in semantic_dict:
-#                 semantic_dict[sentence[i]] = {}
-#                 called_words = []
-                
-#                 for j in range(len(sentence)):    
-#                     if sentence[i] != sentence[j] and (sentence[j] not in called_words):
-#                         if sentence[j] not in semantic_dict[sentence[i]]:
-#                             semantic_dict[sentence[i]][sentence[j]] = 1
-#                         else:        
-#                             semantic_dict[sentence[i]][sentence[j]] += 1
-#                     called_words.append(sentence[j])
-
-#     return semantic_dict
                 
 def build_semantic_descriptors_from_files(filenames):
     sentences = []
@@ -131,27 +68,25 @@ def build_semantic_descriptors_from_files(filenames):
             sentence_split[i] = sentence_split[i].split(" ")
         sentences.extend(sentence_split)
 
+    ## Remove empty strings from the list sentences
     # sentences = list(filter(lambda sentence: sentence != [''], sentences))
     # sentences = [list(filter(None, sentence)) for sentence in sentences
     sentences = [list(filter(None, sentence)) for sentence in list(filter(lambda sentence: sentence != [''], sentences))]
     # for i in sentences:
     #     print(i)
-    
-    
-
     return build_semantic_descriptors(sentences)
-#'exception': {'sexual': 1, 'everywhere': 1, 'computer': 1, 'suddenly': 1, 'ignore': 1, 'occupation': 1}
-#'exception': {'computer': 2, 'sexual': 2, 'suddenly': 2, 'everywhere': 2, '': 3, 'occupation': 1, 'ignore': 1}
-
-#'million': {'adjustment': 3, 'something': 3, 'theory': 3, 'motion': 3, 'condition': 1, 'honor': 1, 'saving': 1, 'meeting': 1, 'basketball': 1, 'authority': 1, 'since': 1, 'anxiety': 1, 'homeless': 1}
-#'million': {'something': 6, 'theory': 9, 'motion': 6, 'adjustment': 6, '': 13, 'basketball': 2, 'meeting': 1, 'condition': 2, 'honor': 1, 'saving': 2, 'since': 1, 'authority': 3, 'homeless': 2, 'anxiety': 1}
-
 
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
-    similarity_score = [similarity_fn(semantic_descriptors[word],semantic_descriptors[choice]) for choice in choices]
+    def vec_similarity(word, choice):
+        try:
+            return similarity_fn(semantic_descriptors[word], semantic_descriptors[choice])
+        except:
+            return -1
+
+    similarity_score = [vec_similarity(word, choice) for choice in choices]
+  
     i_max = similarity_score.index(max(similarity_score)) # Will this return the element that appears first in case of a Tie?
     return choices[i_max]                                 # Also need to add a condition such that if the semantic similarity cannot be computed the similarity must be -1.
-
 
 def run_similarity_test(filename, semantic_descriptors, similarity_fn):
     file = open(filename, "r", encoding = "latin1")
@@ -160,12 +95,9 @@ def run_similarity_test(filename, semantic_descriptors, similarity_fn):
     
     # CHECK THAT THIS DOESNT PRODUCE EMPTY STRING LINES
     # Might work: lines = filter(None, lines)
-    for line in lines:
-        print(line)
-    print(lines)
-    #['draw paint walk paint\n', 'duty task task example\n', 'earnest serious seri...s amusing\n', 'picture painting pai...ing chair\n', 'vexed annoyed amused annoyed\n', 'watch see hear see\n', 'tidy clean mess clean\n', 'youthful young young complex\n', 'strike beat beat complain\n', 'tearful crying frown...ng crying\n', 'lonely alone alone together\n', 'ardent keen keen wise\n', 'thief robber robber postman\n', 'authentic genuine ge...ine false\n', ...]
+
     for i in range(len(lines)):
-        lines[i] = lines[i].replace('\n', "").split(" ")     # Default space
+        lines[i] = lines[i].split()     # Default splits space and new line
     score = 0
     for question in lines:
         if question[1] == most_similar_word(question[0], question[2:], semantic_descriptors, similarity_fn):
@@ -194,8 +126,11 @@ if __name__ == "__main__":
     end_time = time.time()
     dt = end_time - start_time
     
+    # for key, val in semantic_dict.items():
+    #     print(key, ':' val)
+    
     #print(semantic_dict)
-    print()
+    print('###')
     print("Percentage:", run_similarity_test('test.txt', semantic_dict, cosine_similarity))
-    print(dt)
+    print('Time:', dt)
     
